3cfac9f1db39b25258f1395adc3688772b25704c
FPLTomeczek <tomaszs18m@gmail.com>
2023-05-24 10:54:00 +0200

    proxy_test_players

--- src/components/features/transfer_planner/__tests__/transferPlanner.test.jsx
+++ src/components/features/transfer_planner/__tests__/transferPlanner.test.jsx
@@ -8,56 +8,35 @@ import { Provider } from "react-redux";
 import PlayersList from "../list/PlayersList";
 import configureStore from "redux-mock-store";
 
-it("team players are fetched", async () => {
-  const data = await getManagerTeam(7770);
-  expect(data).toHaveLength(15);
+describe("fetching players", () => {
+  it("team players are fetched", async () => {
+    const data = await getManagerTeam(7770);
+    expect(data).toHaveLength(15);
+  });
 });
 
 describe("list filtering", () => {
   const mockStore = configureStore();
+
+  const players = [
+    { id: 1, web_name: "Raya", team: "BRE" },
+    { id: 2, web_name: "Trippier", team: "NEW" },
+    { id: 3, web_name: "Estupiñán", team: "BHA" },
+  ];
+
+  const proxyHandler = {
+    get(obj, prop) {
+      return prop in obj ? obj[prop] : "";
+    },
+  };
+
   it("should return players from selected team", async () => {
     const store = mockStore({
       players: {
         playersList: [
-          {
-            _id: "6464be106deb8d3ee8afad07",
-            id: 81,
-            web_name: "Raya",
-            goals: 0,
-            assists: 0,
-            team: "BRE",
-            element_type: "GK",
-            total_points: 156,
-            now_cost: 48,
-            __v: 0,
-            position: 0,
-          },
-          {
-            _id: "6464be106deb8d3ee8afaeac",
-            id: 357,
-            web_name: "Trippier",
-            goals: 0,
-            assists: 7,
-            team: "NEW",
-            element_type: "DEF",
-            total_points: 179,
-            now_cost: 61,
-            __v: 0,
-            position: 1,
-          },
-          {
-            _id: "6464be106deb8d3ee8afad44",
-            id: 586,
-            web_name: "Estupiñán",
-            goals: 0,
-            assists: 7,
-            team: "BHA",
-            element_type: "DEF",
-            total_points: 123,
-            now_cost: 49,
-            __v: 0,
-            position: 2,
-          },
+          new Proxy(players[0], proxyHandler),
+          new Proxy(players[1], proxyHandler),
+          new Proxy(players[2], proxyHandler),
         ],
         status: "success",
         error: null,
